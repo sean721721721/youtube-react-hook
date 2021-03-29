582aea22089b78142fb196640586d8be435bef98
Sean <f4555xu87@gmail.com>
2021-03-29 18:43:07 +0800

    add side bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, {} from 'react';
 import HeaderNav from './containers/HeaderNav/HeaderNav';
+import SideBar from './containers/SideBar/SideBar';
 
 const App = () => {
   return (
-    <HeaderNav/>
+    <React.Fragment>
+      <HeaderNav/>
+      <SideBar/>
+    </React.Fragment>
   )
 }
 
