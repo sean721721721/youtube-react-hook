56855cb16542eceecdd6ea58d201877223f67f89
Sean <f4555xu87@gmail.com>
2021-03-30 15:24:14 +0800

    adding router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import HeaderNav from './containers/HeaderNav/HeaderNav';
-import SideBar from './containers/SideBar/SideBar';
 import AppLayout from './components/AppLayout/AppLayout';
 import Home from './containers/Home/Home';
+import {Route, Switch} from 'react-router-dom';
+import Watch from './containers/Watch/Watch';
 
 const App = () => {
   return (
     <AppLayout>
-      <Home/>
+      <Switch>
+        <Route path="/watch" component={Watch}/>
+        <Route path="/" component={Home}/>
+      </Switch>
     </AppLayout>
   )
 }
---
+++ src/containers/Watch/Watch.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Watch.scss';
+
+const Watch = () => {
+    return (
+        <div>Watch component goes here</div>
+    )
+}
+
+export default Watch;
