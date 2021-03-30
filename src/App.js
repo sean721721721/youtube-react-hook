d0514aa79ec6d6ac3f0d10362abba9d631acd49f
Sean <f4555xu87@gmail.com>
2021-03-30 15:08:37 +0800

    add AppLayout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import HeaderNav from './containers/HeaderNav/HeaderNav';
 import SideBar from './containers/SideBar/SideBar';
+import AppLayout from './components/AppLayout/AppLayout';
 import Home from './containers/Home/Home';
 
 const App = () => {
   return (
-    <React.Fragment>
-      <HeaderNav/>
-      <SideBar/>
+    <AppLayout>
       <Home/>
-    </React.Fragment>
+    </AppLayout>
   )
 }
 
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import VideoGrid from '../../components/VideoGrid/VideoGrid';
+import SideBar from '../SideBar/SideBar';
 import './Home.scss';
 
 const Home = () => {
     return (
-        <div className="home">
-            <div className="responsive-video-grid-container">
-                <VideoGrid title="Trending"/>
-                <VideoGrid title="Autos & Vehicles" hideDivider={true}/>
+        <React.Fragment>
+            <SideBar/>
+            <div className="home">
+                <div className="responsive-video-grid-container">
+                    <VideoGrid title="Trending"/>
+                    <VideoGrid title="Autos & Vehicles" hideDivider={true}/>
+                </div>
             </div>
-        </div>
+        </React.Fragment>
     )
 }
 
