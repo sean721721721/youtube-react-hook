48cbd7182edcccdff6c08365de332d6471e84dad
Sean <f4555xu87@gmail.com>
2021-03-30 13:45:21 +0800

    video preview grid

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoPreview from '../../components/VideoPreview/VIdeoPreview';
+import './Home.scss';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <VideoPreview/>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/containers/SideBar/SideBarItem/__tests__/SideBarItem.unit.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import SideBarItem from '../SideBarItem';
+
+describe('SideBarItem', () => {
+    test('renders empty SideBarItem', () => {
+        const wrapper = shallow(
+            <SideBarItem/>
+        )
+        expect(wrapper).toMatchSnapshot();
+    });
+    
+    test('renders highlighted SideBarItem that navigates to /feed/trending', () => {
+        const wrapper = shallow(
+            <SideBarItem highlighted icon="fire" label="Trending"/>
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+    
+    test('renders non-highlighted SideBarItem that navigates to /feed/trending', () => {
+        const wrapper = shallow(
+            <SideBarItem icon="fire" label="Trending" />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('renders highlighted SideBarItem with no navigation', () => {
+        const wrapper = shallow(
+            <SideBarItem highlighted icon="fire" label="Trending" />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+    
+    
+});
