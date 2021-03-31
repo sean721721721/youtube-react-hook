7fe97c292a40ae4f57dacb15e961481d8c24d3c2
Sean <f4555xu87@gmail.com>
2021-03-31 23:15:54 +0800

    setting up Most popular videos per category reducer

--- src/store/reducers/videos.js
+++ src/store/reducers/videos.js
@@ -1,4 +1,4 @@
-import {MOST_POPULAR, VIDEO_CATEGORIES} from '../actions/video';
+import {MOST_POPULAR, MOST_POPULAR_BY_CATEGORY, VIDEO_CATEGORIES} from '../actions/video';
 import {SUCCESS} from '../actions';
 
 const initialState = {
@@ -14,6 +14,8 @@ export default function videos(state = initialState, action) {
             return reduceFetchMostPopularVideos(action.response, state);
         case VIDEO_CATEGORIES[SUCCESS]:
             return reduceFetchVideoCategories(action.response, state);
+        case MOST_POPULAR_BY_CATEGORY[SUCCESS]:
+            return reduceFetchMostPopularVideosByCategory(action.response, action.categories, state);
         default:
             return state;
     }
@@ -52,4 +54,48 @@ function reduceFetchVideoCategories(response, prevState) {
         ...prevState,
         categories: categoryMapping,
     };
+}
+
+function reduceFetchMostPopularVideosByCategory(response, categories, prevState) {
+    let videoMap = {};
+    let byCategoryMap = {};
+
+    response.forEach((response, index) => {
+        if (response.status === 400) return;
+
+        const categoryId = categories[index];
+        const {byId, byCategory} = groupVideosByIdAndCategory(response.result);
+        videoMap = {...videoMap, ...byId};
+        byCategoryMap[categoryId] = byCategory;
+    
+    });
+
+    return {
+        ...prevState,
+        byId: {...prevState.byId, ...videoMap},
+        byCategory: {...prevState.byCategory, ...byCategoryMap},
+    };
+}
+
+function groupVideosByIdAndCategory(response) {
+    const videos = response.items;
+    const byId = {};
+    const byCategory = {
+        totalResults: response.pageInfo.totalResults,
+        nextPageToken: response.nextPageToken,
+        items: [],
+    };
+
+    videos.forEach((video) => {
+        byId[video.id] = video;
+
+        const items = byCategory.items;
+        if (items && items) {
+            items.push(video.id);
+        } else {
+            byCategory.items = [video.id];
+        }
+    });
+
+    return {byId, byCategory};
 }
