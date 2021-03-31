7cabca5899ffa196cd61543760bdcc8c82a524dc
Sean <f4555xu87@gmail.com>
2021-03-31 10:55:52 +0800

    adding related videos component

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Watch.scss';
 import Video from '../../components/Video/Video';
+import VideoPreview from '../../components/VideoPreview/VIdeoPreview';
+import RelatedVideos from '../../components/RelatedVideos/RelatedVideos';
 
 const Watch = () => {
     return (
-        <div>
-            <Video id="-7fuHEEmEjs"/>
-        </div>
+        <RelatedVideos/>
     )
 }
 
